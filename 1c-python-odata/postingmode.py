ee1c3c08b4134dd2957104e072c31a61f5b719ed
belov38 <belov38@gmail.com>
2019-06-16 23:59:03 +0800

    Document posting modes

---
+++ 1c-python-odata/postingmode.py
@@ -0,0 +1,9 @@
+class PostingMode:
+    """This object represents a 1C document posting modes ."""
+
+    UNPOST = 'UNPOST'
+    """:obj:`str`: 'Markdown'"""
+    POST = 'POST'
+    """:obj:`str`: 'HTML'"""
+    OPER = 'OPER'
+    """:obj:`str`: 'HTML'"""
