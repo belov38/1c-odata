fae96a57f004526997b1737f68b9c087b220c705
belov38 <belov38@gmail.com>
2019-06-17 23:24:24 +0800

    catalog actions implemented

--- odata1c/catalog.py
+++ odata1c/catalog.py
@@ -9,9 +9,11 @@ from odata1c.utils import make_url_part
 class Catalog:
     infobase: Infobase
 
-    def __init__(self, infobase):
+    def __init__(self, infobase, catname):
         self.infobase = infobase
-        
+        self.catname = catname
+        self.url = self.infobase._full_url.format(obj='Catalog_'+self.catname)
+
     # def new_catalog(self):
     #     # TODO new_catalog
     #     pass
@@ -20,22 +22,42 @@ class Catalog:
     #     # TODO edit_catalog
     #     pass
 
-    # def get_catalog(self):
-    #     # TODO get_catalog
-    #     pass
+    def get(self, guid, select=None):
+        obj = "Catalog_{}(guid'{}')".format(self.catname, guid)
+        url = self.infobase._full_url.format(obj=obj)
+        _url_select = make_url_part('select', select, str)
+        url = url + _url_select
+        r = requests.get(url, auth=self.infobase._auth,
+                         headers=self.infobase._headers)
+        if(r.status_code != 200):
+            raise Exception(r.text)
+        return json.loads(r.text)
 
-    # def get_catalogs(self):
-    #     # TODO get_catalogs
-    #     pass
+    def query(self, top=None, skip=None, select=None, odata_filter=None):
+        _url_top = make_url_part('top', top, int)
+        _url_skip = make_url_part('skip', skip, int)
+        _url_select = make_url_part('select', select, str)
+        _url_filter = make_url_part('filter', odata_filter, str)
+        url = self.url + _url_top + _url_skip + _url_select + _url_filter
+        r = requests.get(url, auth=self.infobase._auth,
+                         headers=self.infobase._headers)
+        if(r.status_code != 200):
+            raise Exception(r.text)
+        return json.loads(r.text)['value']
 
-    # def InformationRegister_slicelast(self):
-    #     # TODO InformationRegister_slicelast
-    #     pass
+    def create(self, data):
+        r = requests.post(self.url, auth=self.infobase._auth,
+                          headers=self.infobase._headers, data=json.dumps(data))
+        if r.status_code != 201:
+            raise Exception(r.text)
+        return json.loads(r.text)
 
-    # def AccountingRegister_balance(self):
-    #     # TODO AccountingRegister_balance
-    #     pass
+    def edit(self, guid, data):
+        obj = "Catalog_{}(guid'{}')".format(self.catname, guid)
+        url = self.infobase._full_url.format(obj=obj)
+        r = requests.patch(url, auth=self.infobase._auth,
+                           headers=self.infobase._headers, data=json.dumps(data))
+        if(r.status_code != 200):
+            raise Exception(r.text)
 
-    # def AccumulationRegister_balance(self):
-    #     # TODO AccumulationRegister_balance
-    #     pass
+        return json.loads(r.text)
