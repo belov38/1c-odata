86e11d2ff202107871e6b86f2e08483a9d8220f6
kwbs <45818533+kwbs@users.noreply.github.com>
2019-06-27 17:55:37 +1200

    add slice_last

--- odata1cw/informationregister.py
+++ odata1cw/informationregister.py
@@ -28,6 +28,20 @@ class InformationRegister:
             raise Exception(r.text)
         return json.loads(r.text)['value']
 
-    def slice_last(self):
-        # TODO slice_last
-        pass
+    def slice_last(self, **kwargs):
+        select_value = kwargs.get('select')
+        _url_select = make_url_part('select', select_value, str)
+        orderby_value = kwargs.get('orderby')
+        _url_orderby = make_url_part('orderby', orderby_value, str)
+        expand_value = kwargs.get('expand')
+        _url_expand = make_url_part('expand', expand_value, str)
+        period_value = '' if kwargs.get('period') is None else kwargs.get('period')
+
+        full_url = (self.infobase._full_url.format(
+            obj='InformationRegister_'+self.regname+f'/SliceLast({period_value})'))+f'{_url_select}{_url_orderby}{_url_expand}'
+
+        r = requests.get(full_url, auth=self.infobase._auth,
+                         headers=self.infobase._headers)
+        if(r.status_code != 200):
+            raise Exception(r.text)
+        return json.loads(r.text)    
--- samples/catalog_item.py
+++ samples/catalog_item.py
@@ -3,8 +3,17 @@ from odata1cw.postingmode import PostingMode
 from datetime import datetime
 from odata1cw.document import Document
 from odata1cw.catalog import Catalog
+from odata1cw.informationregister import InformationRegister
 
-connection = Infobase('http://localhost/','Infobase1','User','123')
+
+connection = Infobase('http://localhost/', 'Infobase1', 'User', '123')
 cpl = Document(connection, 'Пользователи')
 users = cpl.query(top=3)
 print(users)
+
+# example slise last
+inforeg = InformationRegister(connection, 'OdometerReadings')
+period_value = f"Period = datetime'{datetime.now().isoformat()}'"
+res = inforeg.slice_last(
+    period=period_value, orderby="Reading desc", expand='Auto',)
+print(res)
